a5a4658b4e09e2a748115dfeb8f57d561bfd1897
SpiritsUndead <144187397+SpiritsUndead@users.noreply.github.com>
2024-02-27 17:18:42 -0500

    Reinstate "HM use Stuff"
    Im an idiot, this apparently was always working?

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1285,7 +1285,7 @@ bool8 PartyHasMonWithSurf(void)
         {
             if (GetMonData(&gPlayerParty[i], MON_DATA_SPECIES) == SPECIES_NONE)
                 break;
-            if (MonKnowsMove(&gPlayerParty[i], MOVE_SURF))
+            if (CanLearnTeachableMove(GetMonData(&gPlayerParty[i], MON_DATA_SPECIES_OR_EGG), MOVE_SURF) == TRUE)
                 return TRUE;
         }
     }
